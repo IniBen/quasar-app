e5d767da6a07ecc3a7a4c805fac9dcfe8463bf40
Iniubong Ben <iniwinben@live.com>
2021-12-19 18:41:49 +0100

    first commit

---
+++ Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est, perspiciatis voluptate. Rerum ipsam et hic doloribus recusandae quae ullam, officiis culpa at necessitatibus, dolores unde pariatur eum voluptatibus magni adipisci?</p><br>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam fugiat, odit totam minima impedit nobis, facere dignissimos inventore quam iste saepe cum dolores voluptatibus dolor incidunt eveniet praesentium beatae assumenda.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  
+})
+</script>
---
+++ Todo.vue
@@ -0,0 +1,61 @@
+<template>
+  <q-page class="bg-grey-3 column">
+  <div class="q-pa-md">
+    <div class="q-gutter-sm">
+    <q-list class="bg-white">
+      <q-item
+      v-for="task in tasks" 
+      :key="task.title" 
+      @click="task.done = !task.done"
+      :class="{'done' : task.done}"
+      onclick="clickMethod"
+      tag="label" v-ripple>
+        <q-item-section avatar>
+          <q-checkbox v-model="task.done" class="no-pointer-events" color="teal" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }} </q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+    </div>
+
+    <div class="q-px-sm q-mt-sm">
+      Your selection is: <strong>{{ color }}</strong>
+    </div>
+  </div>
+  </q-page>
+</template>
+
+<script>
+// import { defineComponent } from 'vue';
+
+export default ({
+  data() {
+    return {
+      tasks:[
+        {
+          title: 'create login for ipay',
+          done : false
+        },
+        {
+          title: 'push login for ipay',
+          done: false
+        },
+        {
+          title: 'create login for newdevz',
+          done: false
+        }
+      ]
+    }
+  } 
+})
+</script>
+
+<style land="SCSS">
+  .done {
+    .q-item__label {
+      text-decoration: line-through;
+    }
+  }
+</style>
